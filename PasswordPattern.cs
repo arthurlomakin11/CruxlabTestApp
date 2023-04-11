c6e909c82885f609d95b66359e5b24c5ec00477f
Artur <arthurlomakin11@gmail.com>
2023-04-11 18:37:03 +0300

    Initial commit

---
+++ PasswordPattern.cs
@@ -0,0 +1,30 @@
+﻿namespace CruxlabTestApp;
+
+internal class PasswordPattern
+{
+    private readonly char _letter;
+    private readonly string _password;
+    private readonly int _numberPatternStart;
+    private readonly int _numberPatternEnd;
+
+    public PasswordPattern(string fileString)
+    {
+        var fileStringArray = fileString.Split(" ");
+        
+        _letter = char.Parse(fileStringArray[0]);
+        _password = fileStringArray[2];
+    
+        var numberPatternString = fileStringArray[1][..^1]; // cut latest symbol
+        _numberPatternStart = numberPatternString[0] - '0'; // convert to char to integer
+        _numberPatternEnd = numberPatternString[2] - '0';
+    }
+
+    public bool IsPasswordMatching()
+    {
+        return Enumerable.Range(_numberPatternStart, _numberPatternEnd - _numberPatternStart + 1).Any(numberPattern =>
+        {
+            var match = _password.Count(c => c == _letter);
+            return match == numberPattern;
+        });
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using CruxlabTestApp;
+
+var fileStrings = File.ReadAllLines("passwords.txt");
+
+var matchedPasswordsCount= fileStrings.Count(fileString => new PasswordPattern(fileString).IsPasswordMatching());
+Console.WriteLine(matchedPasswordsCount);
